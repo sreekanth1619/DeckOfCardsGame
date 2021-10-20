cee185f9f19c603cf2e418f6bdf7d8e9c4d33bb4
sreekanth1619 <87491860+sreekanth1619@users.noreply.github.com>
2021-10-20 19:44:02 +0530

    Added: Initializing the size of the deckofcards

---
+++ DeckOfCards/src/deckofcards/DeckOfCardsMain.java
@@ -0,0 +1,10 @@
+package deckofcards;
+
+public class DeckOfCardsMain {
+	  public static void main(String[] args) {
+		  //creating the object to call the methods performed in the DeckOfCards game
+	        DeckOfCardsGame deckOfCardsGame = new DeckOfCardsGame();
+	        deckOfCardsGame.welcome();
+	        deckOfCardsGame.deckOfCards();
+ }
+ }
