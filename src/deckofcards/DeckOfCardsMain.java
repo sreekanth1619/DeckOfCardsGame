4e8c6abafb63fc0229c02de452b0a49dde9fa651
sreekanth1619 <87491860+sreekanth1619@users.noreply.github.com>
2021-10-29 12:57:11 +0530

    Added: There should be minimum 2 and maximum 4 players in the Game

--- src/deckofcards/DeckOfCardsMain.java
+++ src/deckofcards/DeckOfCardsMain.java
@@ -2,9 +2,13 @@ package deckofcards;
 
 public class DeckOfCardsMain {
 	  public static void main(String[] args) {
-		  //creating the object to call the methods performed in the DeckOfCards game
+		    /**
+	        In this I have created an object of DeckOfCardsGame class and just calling the method through object.
+	         */
 	        DeckOfCardsGame deckOfCardsGame = new DeckOfCardsGame();
 	        deckOfCardsGame.welcome();
 	        deckOfCardsGame.deckOfCards();
+	        deckOfCardsGame.noOfPlayers();
+	        
  }
  }
